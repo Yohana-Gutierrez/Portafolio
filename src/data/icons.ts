731d824c973c43a84efec9cd84833c06fbbb59ce
Yohana-Gutierrez <aygutierrezl@unah.hn>
2024-10-30 00:50:39 -0600

    Corregir  Header.

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,11 +1,13 @@
 // src/data/icons.ts
 
+// src/icons.ts
 export enum TechIcons {
-    Python = "fab fa-python",
-    Java = "fab fa-java",
-    Firebase = "fab fa-fire", 
-    // Agrega más iconos según lo necesites
+    Python = 'python-icon-url',
+    Firebase = 'firebase-icon-url',
+    Java = 'java-icon-url',
+    // Agrega otros íconos aquí
 }
+
 
 // Función para obtener el ícono basado en el nombre
 export const getIcon = (iconName: TechIcons) => {
