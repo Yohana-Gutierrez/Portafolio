6d8becc7063797afd2a91c38bb8ac2f58e7cb47c
Yohana-Gutierrez <aygutierrezl@unah.hn>
2024-11-04 11:26:01 -0600

    iconos

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,15 +1,13 @@
 // src/data/icons.ts
 
-// src/icons.ts
-export enum TechIcons {
-    Python = 'python-icon-url',
-    Firebase = 'firebase-icon-url',
-    Java = 'java-icon-url',
-    // Agrega otros íconos aquí
-}
+// Objeto simple que mapea nombres de tecnologías a URLs de iconos
+export const TechIcons = {
+    Python: 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png',
+    Firebase: 'https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png',
+    Java: 'https://banner2.cleanpng.com/20181109/oll/kisspng-java-development-kit-logo-programming-language-por-java-logo-svg-1713924793188.webp',
+};
 
-
-// Función para obtener el ícono basado en el nombre
-export const getIcon = (iconName: TechIcons) => {
-    return iconName;
+// Función para obtener la URL del ícono basado en el nombre de la tecnología
+export const getIcon = (iconName: keyof typeof TechIcons): string => {
+    return TechIcons[iconName];
 };
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,36 +1,28 @@
-import { TechIcons } from './icons.ts'; // Verifica que esta ruta sea correcta
+import { TechIcons } from './icons';
 
-export type Project = {
-    title: string;
-    description: string;
-    techs: TechIcons[];
-    link: string;
-    isComingSoon?: boolean;
-};
-
-export const projects: Project[] = [
+export const projects = [
     {
         title: "Tres Rayas en uno",
         description: "Acortador de URL con Base de Datos y arquitectura hexagonal",
-        techs: [TechIcons.Python],
+        techs: ["Python"], // Usa las claves como strings
         link: "https://github.com/Yohana-Gutierrez/Programacion2/tree/master/Capitulo%208/TresEnraya",
     },
     {
         title: "Tour del Caballero",
         description: "Algoritmo muy conocido",
-        techs: [TechIcons.Python],
+        techs: ["Python"],
         link: "https://github.com/Yohana-Gutierrez/AED-/tree/main/CAPITULO_14",
     },
     {
         title: "Tic_Tac_Toe",
         description: "El famoso juego de x-0",
-        techs: [TechIcons.Python], // Añade `techs` en el lugar correcto
+        techs: ["Python"],
         link: "https://github.com/Yohana-Gutierrez/POO/tree/main/tic-tac-toe-5Componentes",
     },
     {
         title: "Terrapez App",
         description: "Reserva de restaurante con Base de Datos",
-        techs: [TechIcons.Firebase, TechIcons.Java],
+        techs: ["Firebase", "Java"],
         link: "https://github.com/Yohana-Gutierrez/POO/tree/main/terrapez%20Restaurante-ProyectoFinal",
     },
 ];
